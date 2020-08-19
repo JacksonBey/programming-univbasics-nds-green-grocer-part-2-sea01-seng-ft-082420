2b0ed80a846f0769f47a5e46e7952b4be073259e
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-19 04:20:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,16 +1,46 @@
 require_relative './part_1_solution.rb'
-
+require 'pry'
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ccart = consolidate_cart(cart)
+i=0
+if coupons===[]
+  ccart[0][:count]+=1
+  return ccart
 end
+ccart.each do |key|
+  coupons.each do |ckey|
+    coupitem={}
+    if ckey[:item]===key[:item]
+      if key[:count]>ckey[:num]
+        key[:count]-=ckey[:num]
+        coupitem.merge! :item => "#{key[:item]} W/COUPON",:price => ckey[:cost].to_f/ckey[:num],:clearance => key[:clearance],:count => ckey[:num]
+        ccart.push coupitem
+      else if key[:count]<=ckey[:num]
+        coupitem.merge! :item => "#{key[:item]} W/COUPON",:price => ckey[:cost].to_f/ckey[:num],:clearance => key[:clearance],:count => ckey[:num]
+        ccart.push coupitem
+        key[:count]-=ckey[:num]
+        if (key[:count]-ckey[:num])<0
+          key[:count]=0
+        end
+      end
+end
+end
+end
+end
+return ccart
+end
+
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ccart = consolidate_cart(cart)
+ccart.each do |key|
+  if key[:clearance]
+    key[:price]=key[:price]-(key[:price]/(1.2))
+    binding.pry
+  end
 end
+end
+
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
