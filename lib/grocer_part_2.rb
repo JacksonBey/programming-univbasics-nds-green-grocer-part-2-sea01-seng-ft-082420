708af85cdc704b1b7bd66e64d0cc6f3ba3d00659
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-19 04:34:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -44,9 +44,6 @@ end
 def checkout(cart, coupons)
 ccart=consolidate_cart(cart)
 vcart=apply_coupons(ccart, coupons)
-  if coupons.length===1 && ccart.length>1
-    binding.pry
-  end
 jcart=apply_clearance(vcart)
   if coupons.length===1 && ccart.length>1
     binding.pry
